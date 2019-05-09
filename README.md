ec35ff81dd1a640fb5a341348f60a11f3768ead2
tang03130313 <smile03131@gmail.com>
2019-05-09 23:18:38 +0800

    ReactJS

---
+++ README.md
@@ -0,0 +1,11 @@
+Nursing Home
+====
+ReactJS NodeJS MySQL HighCHart
+-------
+# 前端 
+## ReactJS
+## Redux
+## HighCHart
+# 後端
+## NodeJS
+## MySQL
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize")
+const db = require("../database/db")
+
+module.exports = db.sequelize.define(
+    'user',
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        username: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    },
+    {
+        timestamps: false
+    }
+)
